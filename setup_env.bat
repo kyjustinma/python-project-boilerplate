654b817e0c4f0208400b866fc998d1d4bc2ceece
Justin_Ma <justin.ma@roborn.com>
2022-09-06 11:22:15 +0800

    fix: setup.bat

--- setup_env.bat
+++ setup_env.bat
@@ -5,4 +5,12 @@ echo Current Path:  %mypath%
 set /p env_name= Enter name of conda environment:
 
 cd /d %mypath%
-start cmd.exe /k "conda env create --name %env_name% -f requirements.yml && conda activate %env_name% && conda install --yes -c conda-forge pre_commit && pre-commit install"
+
+@REM Installs Env from requirements.yml and installs precommit
+call conda env create --name %env_name% -f requirements.yml
+call conda activate %env_name%
+call conda install --yes -c conda-forge pre_commit
+call pre-commit install
+
+echo Finished setting up conda env and pre-commit
+pause
