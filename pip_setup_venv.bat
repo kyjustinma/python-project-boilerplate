2671d85151960c9db8503eede02cfe1062143634
Justin Ma <63966698+justma360@users.noreply.github.com>
2022-09-28 10:12:00 +0800

    Update pip_setup_venv.bat

--- pip_setup_venv.bat
+++ pip_setup_venv.bat
@@ -9,7 +9,7 @@ cd /d %mypath%
 @REM Installs Env from requirements.yml and installs precommit
 call python -m venv %env_name%
 call %env_name%\Scripts\activate
-call pip install requirements.txt
+call pip install -r requirements.txt
 call pre-commit install
 call pre-commit autoupdate
 call pre-commit install --hook-type commit-msg
