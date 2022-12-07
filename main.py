2d5fffb3bff2ebc23464fdb8a56dc27e9020eceb
Justin_Ma <justin.ma@roborn.com>
2022-12-07 15:23:49 +0800

    style: update exit to resume command line

--- main.py
+++ main.py
@@ -7,18 +7,24 @@ from sample.input_args import parse_arguments
 from config.settings import config, logger
 
 
+def exit_functions():
+    print("Goodbye cruel world")
+
+
 def exit_gracefully(signum, frame):
     # restore the original signal handler as otherwise evil things will happen
     # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
     signal.signal(signal.SIGINT, original_sigint)
     try:
         if input("\n Do you really want to quit? (y/n) >").lower().startswith("y"):
-            logger.warn("Application ended gracefully")
-            sys.exit(1)
+            logger.warning("Application ended gracefully")
+            exit_functions()
+            os._exit(0)
 
     except KeyboardInterrupt:
-        print("Ok ok, quitting")
-        sys.exit(1)
+        print("Keyboard interrupt, quitting service")
+        exit_functions()
+        os._exit(1)
 
     # restore the exit gracefully handler here
     signal.signal(signal.SIGINT, exit_gracefully)
