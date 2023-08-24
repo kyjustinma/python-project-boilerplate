854af149fd3c4f16cef8b6753c5627cdced34a48
Justin <maky@shoalter.com>
2023-08-24 18:26:59 +0800

    feat: added some usefull util functions

--- custom_utils/path_utils.py
+++ custom_utils/path_utils.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import json
 
 
 def get_folder_files(path, extension="*.txt", reverse=True):
@@ -7,3 +8,27 @@ def get_folder_files(path, extension="*.txt", reverse=True):
     list_of_files = glob.glob(path)  # * means all if need specific format then *.csv
     list_of_files.sort(key=os.path.getmtime, reverse=reverse)
     return list_of_files
+
+
+def check_create_folder(path):
+    if os.path.exists(path):
+        return path
+    else:
+        os.makedirs(path)  # Creates all sub directories as needed
+        return path
+
+
+def check_create_file(path):
+    if os.path.exists(path):
+        return path
+    else:
+        folder = os.path.dirname(path)
+        check_create_file(folder)  # make sure folder for file exists
+        with open(path, "w") as _:  # create empty new file
+            pass
+
+
+def json_to_dict(path):
+    with open(path) as json_file:
+        json_data = json.load(json_file)
+    return json_data
